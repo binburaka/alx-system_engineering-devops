187d90d30508f32e46e29fb3b8d77375649b3c3a
Binburaka <ushindiburaka@gmail.com>
2023-05-22 11:53:58 -0700

    done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# using this REST API, for a given employee ID,
-# returns information about his/her TODO list progress.
+"""script using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
 
 import re
 import requests
