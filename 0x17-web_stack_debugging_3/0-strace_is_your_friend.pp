207f34ee271a62e0a154aff33e531ad94fdeef62
binburaka <ushindiburaka@gmail.com>
2023-06-06 19:48:23 +0000

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
-# Puppet manifest to fix misconfiguration in Wordpress
-exec { 'no phpp':
-    command => '/bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php'
-}
-exec {'restart web server':
-  command => '/usr/sbin/service apache2 restart'
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
